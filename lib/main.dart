9bcda49c86842e9a9f278732930108e67ab674d0
EnricoGollner <enricogollner.tech@gmail.com>
2024-09-16 08:33:42 -0300

    v1.0

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:live_tracker_app/order_traking_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+      ),
+      home: const OrderTrackingPage(),
+    );
+  }
+}
